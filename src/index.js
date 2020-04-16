4df38c379f509e3f1938354f1c5e2547df7f7603
ruben <cameroamadorr@gmail.com>
2020-04-16 18:20:34 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './Pages/App'
-import 'bootstrap/dist/css/bootstrap.css'
-const container = document.getElementById('root')
-ReactDOM.render(<App/>,container)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Pages/App';
+import 'bootstrap/dist/css/bootstrap.css';
+const container = document.getElementById('root');
+ReactDOM.render(<App/>,container);
